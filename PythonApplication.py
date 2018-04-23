cffa3ab47ee7869c155a95be27d446ba92823a3f
Andrew Hernandez <jandrewh@outlook.com>
2018-04-22 19:52:11 -0700

    Update PythonApplication.py

--- PythonApplication.py
+++ PythonApplication.py
@@ -1,23 +1,27 @@
 #reminder: create genericlist class / definition to occupy the class account
 #reminder: create a function to input new accounts
 
-class Values(object):
-    def genericlist():
-        lID = "testingID"
-        lPW = "password"
+
+def genericlist(loginID, loginPW, firstname, lastname, birthday, emailaddress):
+        loginID = "testingID"
+        loginPW = "password"
         firstname = "Andrew"    
         lastname = "Hernandez" 
         birthday = "June 14 1999"
-        email = "jonandrew@outlook.com"
+        emailaddress = "jonandrew@outlook.com"
 
-
-class Account(genericlist.Values): #upon startup will automatically create a "table" from the genericlist class
-    def __init__(self, lID='0', lPW='0', fName='0', lName='0', birthday='0', email='0'):
-        self.lId = lID
-        self.lPW = lPW
+class Account(object): #upon startup will automatically create a "table" from the genericlist
+    def __init__(self, lID='0', lPW='0', fName='0', lName='0', bday='0', email='0'):
+        self.lID = loginID
+        self.lPW = loginPW
         self.fName = firstname
         self.lName = lastname
-        self.birthday = birthday
-        self.email = email
+        self.bday = birthday
+        self.email = emailaddress
+    def createSubSeenit():
+    def joinSubSeenit():
 
-    def createAccount(): #creates a new account
+class subSeenit(object)
+    def __init__(self, name="0", )
+
+   # def createAccount(): #creates a new account
