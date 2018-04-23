020033faf68716e1863ed6655294ae5ed2221500
Andrew Hernandez <jandrewh@outlook.com>
2018-04-22 20:02:57 -0700

    Update PythonApplication.py

--- PythonApplication.py
+++ PythonApplication.py
@@ -2,26 +2,38 @@
 #reminder: create a function to input new accounts
 
 
-def genericlist(loginID, loginPW, firstname, lastname, birthday, emailaddress):
+def genericlist():
+        #Account generic list
         loginID = "testingID"
         loginPW = "password"
         firstname = "Andrew"    
         lastname = "Hernandez" 
         birthday = "June 14 1999"
         emailaddress = "jonandrew@outlook.com"
+        subseenit = 'SJSU - Subseenit'
+        
+        #subseenit genericlist
+        subseenitName ="SJSU - Subseenit"
+        subseenitCategory = 'Educational'
+
 
 class Account(object): #upon startup will automatically create a "table" from the genericlist
-    def __init__(self, lID='0', lPW='0', fName='0', lName='0', bday='0', email='0'):
+    def __init__(self, lID='0', lPW='0', fName='0', lName='0', bday='0', email='0', subbedSS='0'):
         self.lID = loginID
         self.lPW = loginPW
         self.fName = firstname
         self.lName = lastname
         self.bday = birthday
         self.email = emailaddress
-    def createSubSeenit():
-    def joinSubSeenit():
+        self.subbedSS = subseenit
+   # def createSubSeenit():
+        #...
+    #def joinSubSeenit():
+        #...
 
-class subSeenit(object)
-    def __init__(self, name="0", )
+class subSeenit(object):
+    def __init__(self, name='0', category='0'):
+        self.name = subseenitName
+        self.category = subseenitCategory
 
    # def createAccount(): #creates a new account
