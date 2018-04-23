fdd6dfe9dce3137fa7753bb5c97c1fa5ab1345a8
Andrew Hernandez <jandrewh@outlook.com>
2018-04-22 19:24:38 -0700

    base class

---
+++ PythonApplication.py
@@ -0,0 +1,18 @@
+#reminder: create genericlist class / definition to occupy the class account
+#reminder: create a function to input new accounts
+
+class Values(object):
+    def
+
+
+class Account(genericlist):
+    def __init__(self, lID='0', lPW='0', fName='0', lName='0', birthday='0', email='0'):
+        self.lId = lID
+        self.lPW = lPW
+        self.fName = firstname
+        self.lName = lastname
+        self.birthday = birthday
+        self.email = email
+definition createAccount()
+    #insert code here about creating a new account
+ 
