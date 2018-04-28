c67a78169e3173b1dff24c970b986aad04a19003
Andrew Hernandez <jandrewh@outlook.com>
2018-04-28 14:00:24 -0700

    Update commandline_v1.py

--- commandline_v1.py
+++ commandline_v1.py
@@ -1,10 +1,10 @@
 import os
 import sqlite3 as sql
-#galatians 6:9
+# galatians 6:9
 conn = sql.connect('seenit.db')
 c = conn.cursor()
 
-def build_database():      
+def build_database():
     fd = open('seenit.sql', 'r')
     sqlFile = fd.read()
     fd.close()
@@ -29,8 +29,8 @@ def build_database():
 def print_menu():
         #MENU DESIGN!
         print (33* "-", "MENU" , 33 * "-")
-        print( "|", 30 * " ", "insert user", 30 * " ", "|", )
-        print( "|", 30 * " ", "insert subseenit", 30 * " ", "|", )
+        print( "|", 30 * " ", "insert user", 25 * " ", "|", )
+        print( "|", 30 * " ", "insert subseenit", 20 * " ", "|", )
         print( "|", 30 * " ", "modify", 30 * " ", "|", )
         print( "|", 30 * " ", "delete", 30 * " ", "|", )
         print( "|", 30 * " ", "display tables", 22 * " ", "|", )
@@ -40,7 +40,7 @@ def print_menu():
 def insert_user():
     #insert sql commands go here
     print("you chose insert user")
-    a =input("user id#? (choose your favorite number)")
+    a =input("user id#?")
     x =input("user name?")
     y =input("email?")
     c.execute("INSERT INTO user (u_id,u_name,email) VALUES (?, ?, ?)",
@@ -50,8 +50,12 @@ def insert_user():
 def insert_subseenit():
     #insert sql commands go here
     os.system('clear')
+    a =input("")
+    x =input("user name?")
+    y =input("email?")
+    c.execute("INSERT INTO user (s_id,category,creater_id) VALUES (?, ?, ?)",
+          (a,x,y))
 
-    
 def modify():
     #modify sql commands go here
     os.system('clear')
@@ -60,17 +64,26 @@ def delete():
     #delete sql commands go here
     os.system('clear')
     print("you chose delete")
+    print("please choose from this table a username")
+    display_tables()
+    x = input('Enter the value you want to delete: ')
+    c.execute("DELETE FROM user WHERE u_name =?", (x,))  #Deletes the row where name = x
+
+    #a test function of our delete
 def display_tables():
     #delete sql commands go here
     os.system('clear')
     print("you chose display_tables")
+    c.execute('SELECT * FROM user')
+    [print (row) for row in c.fetchall()]
 
 build_database()
 loop = True
 while loop:
     print_menu()
-   
-    selection = input("\n") #for administrator - enter 0 to exit program
+
+    selection = input("\n")
+    # for administrator - enter 0 to exit program
     if selection  == "insert user":
         insert_user()
     elif selection  == "delete":
@@ -84,4 +97,3 @@ while loop:
         exit()
     else:
        print("we're sorry you entered something not on the menu. Please try again.")
- 
